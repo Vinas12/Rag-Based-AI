b279318843d0166720dded125619a8a5ca4a9ced
Vinas12 <vinasmangroliya12@gmail.com>
2026-07-23 17:04:09 +0530

    Update preprocess_json.py

--- preprocess_json.py
+++ preprocess_json.py
@@ -9,7 +9,7 @@ def create_embedding(texts):
     print("thinking...")#this is the local port of ollama
     r = requests.post("http://localhost:11434/api/embed",json={  #send the post request to your local ollama server,/api/embed endpoint takes a model name and some input text, and returns vector embeddings for that text. 
     "model":"bge-m3",
-    "input":all_text
+    "input": text
     })
 
     
